0da4521e55c279b6c96bad1ba8739b9de22c17c0
shigasy <kxx555u.u@gmail.com>
2020-05-24 05:44:00 +0900

    remove: 色々消した

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,13 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        color-roloc
-      </h1>
-      <h2 class="subtitle">
-        My badass Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <logo />
+    <p>ほげほげ</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Logo from '@/components/Logo.vue'
 
 export default Vue.extend({
   components: {
@@ -37,36 +18,6 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  margin: 0 auto;
-  text-align: center;
-  background: $color-accent;
-
-  @include center-flex;
-}
-
-.title {
-  display: block;
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 100px;
-  font-weight: 300;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  padding-bottom: 15px;
-  font-size: 42px;
-  font-weight: 300;
-  color: #526488;
-  word-spacing: 5px;
-}
-
-.links {
-  padding-top: 15px;
+  color: $color-accent;
 }
 </style>
